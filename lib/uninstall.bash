4ea777cc05cf5458b52d5e11de8d2edc2bffe95c
Wayne E. Seguin <wayneeseguin@gmail.com>
2021-11-18 16:54:01 -0500

    I'm laughing so hard I'm at a loss for words.

--- lib/uninstall.bash
+++ lib/uninstall.bash
@@ -1,10 +1,11 @@
-if [[ -d "${OPT_PATH:="/opt/c3tk"}" ]]
-then 
-  echo "Removing "${OPT_PATH}", /usr/local/bin/c3tk..."
-  sudo rm -rf "${OPT_PATH}" /usr/local/bin/c3tk
-fi
-
-echo "Removing ~/.config/c3tk..."
-rm -rf ~/.config/c3tk
-echo "c3tk uninstalled."
-
+uninstall() {
+  if [[ -d "${OPT_PATH:="/opt/c3tk"}" ]]
+  then 
+    echo "Removing "${OPT_PATH}", /usr/local/bin/c3tk..."
+    sudo rm -rf "${OPT_PATH}" /usr/local/bin/c3tk
+  fi
+  
+  echo "Removing ~/.config/c3tk..."
+  rm -rf ~/.config/c3tk
+  echo "c3tk uninstalled."
+}
