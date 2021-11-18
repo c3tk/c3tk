05a6fd6e6ea8d7172ea1c767fab13e3c8c358536
Wayne E. Seguin <wayneeseguin@gmail.com>
2021-11-18 17:12:26 -0500

    Added update usage function. Added default for rm -rf to ensure can never be / from empty variable.

--- lib/uninstall.bash
+++ lib/uninstall.bash
@@ -17,7 +17,7 @@ uninstall() {
   if [[ -d "${OPT_PATH:="/opt/c3tk"}" ]]
   then 
     echo "Removing "${OPT_PATH}", /usr/local/bin/c3tk..."
-    sudo rm -rf "${OPT_PATH}" /usr/local/bin/c3tk
+    rm -rf "${OPT_PATH}" /usr/local/bin/c3tk
   fi
   
   echo "Removing ~/.config/c3tk..."
--- lib/update.bash
+++ lib/update.bash
@@ -1,5 +1,20 @@
+update_usage() {
+  cat <<-USAGE
+
+Usage:
+
+    c3tk update
+
+Description:
+
+    Updates c3tk from via https://github.com/c3tk/c3tk
+
+USAGE
+exit 0
+}
+
 update() {
   rm -f ${INSTALL_PATH}/c3tk &&
-    rm -rf ${LIB_PATH} &&
+    rm -rf ${LIB_PATH:-"/opt/c3tk/lib"}/* &&
     install
 }
