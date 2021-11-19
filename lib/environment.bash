87127f566f8f7765cf54068106eb0b2a3c65bde2
Wayne E. Seguin <wayneeseguin@gmail.com>
2021-11-19 14:39:03 -0500

    Added find_runtime.

--- lib/environment.bash
+++ lib/environment.bash
@@ -2,6 +2,12 @@ paths() {
   echo "${HOME}/.c3tk/bin:${INSTALL_PATH}"
 }
 
+find_runtime() {
+  local _runtime=$(which docker)
+  [[ -n "${_runtime}" ]] || _runtime=$(which podman)
+  [[ -z "${_runtime}" ]] || echo "${_runtime}"
+}
+
 env_setup() {
   true \
     "${CONFIG_PATH:="$HOME/.c3tk"}" \
@@ -9,7 +15,7 @@ env_setup() {
     "${IMAGE_TAG:="${TAG:-latest}"}" \
     "${DIST:="linux"}" \
     "${ARCH:="amd64"}" \
-    "${RUNTIME:="${RUNTIME:-"$(which docker)"}"}"
+    "${RUNTIME:="${RUNTIME:-"$(find_runtime)"}"}"
 
   command -v ${RUNTIME} &>/dev/null || 
     raise "No runtime found in path, install docker or podman"
