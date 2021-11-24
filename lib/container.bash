07324dc47f5169c56dbd85d916633ad7dfdfba74
Wayne E. Seguin <wayneeseguin@gmail.com>
2021-11-24 09:51:26 -0500

    Set HOME to /u in the container, preserve anything in /root/

--- lib/container.bash
+++ lib/container.bash
@@ -1,5 +1,6 @@
 run_container() {
   local _tty
   [[ "$(tty)" =~ ^not*  ]] && _tty="" || _tty="-t"
-  exec ${RUNTIME} run --rm ${_tty} -w '/w' -v "${PWD}:/w" -v "${HOME}:/root" "$@"
+  exec ${RUNTIME} run --rm ${_tty} -w /w -e HOME=/u \
+    -v "${PWD}:/w" -v "${HOME}:/u" "$@"
 }
