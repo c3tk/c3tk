00c869ce0f6f6606d1f8b17fedfa75095bc156bb
Wayne E. Seguin <wayneeseguin@gmail.com>
2021-11-30 23:18:36 -0500

    Add envs= : a , separated list of env var names to pass through.

--- lib/run.bash
+++ lib/run.bash
@@ -7,12 +7,25 @@ run_cmd() {
   _cmd="${CMD:-$(config_read "${cmd}" "cmd")}"
   _configs=($(config_read "${cmd}" "configs" | tr ',' ' '))
   _mounts=($(config_read "${cmd}" "mounts" | tr ',' ' '))
+  _envs=($(config_read "${cmd}" "envs" | tr ',' ' '))
 
   _c="-i" # Default to allowing interactive
-  grep -q 'stream' "${CONFIG_PATH}/cmds/${cmd}" &&
-    _c="${_c} --log-driver=none -a stdin -a stdout -a stderr "
+  case ${RUNTIME} in
+    (*docker*)
+      grep -q 'stream' "${CONFIG_PATH}/cmds/${cmd}" && _c="${_c} --log-driver=none -a stdin -a stdout -a stderr "
+      ;;
+    (*podman*)
+      _c="${c} -q"
+      ;;
+    (*)
+      ;;
+  esac
 
-  for _m in _mounts
+  for _env in "${_envs[@]}"
+  do  _c="${_c} -e ${_env}='$(printenv ${_env})' "
+  done
+
+  for _m in "${_mounts[@]}"
   do _c="${_c} -v ${_m} "
   done
 
